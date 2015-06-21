b4c3511c5904d2f7258bc912425323242839fa51
John Papa <john@johnpapa.net>
2015-06-21 12:10:10 -0700

    removed all typescript. now just ES6

--- src-ts/app/components/pipes/pipes.js
+++
@@ -1,6 +0,0 @@
-var change_detection_1 = require('angular2/change_detection');
-var currency_1 = require('app/components/pipes/currency');
-exports.pipes = Object.assign({}, change_detection_1.defaultPipes, {
-    'currency': [new currency_1.CurrencyPipe()]
-});
-//# sourceMappingURL=pipes.js.map
--- src-ts/app/components/pipes/pipes.ts
+++
@@ -1,7 +0,0 @@
-import { defaultPipes } from 'angular2/change_detection';
-import { CurrencyPipe } from 'app/components/pipes/currency';
-
-
-export var pipes = Object.assign({}, defaultPipes, {
-  'currency': [new CurrencyPipe()]
-});
